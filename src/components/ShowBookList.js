7ee143f949ce7e6451b46b1559184b7c7827b9cf
gunish-dt <gunish@devtron.ai>
2022-03-08 00:12:13 +0530

    code changes added fetchbook

--- src/components/ShowBookList.js
+++ src/components/ShowBookList.js
@@ -4,6 +4,7 @@ import axios from "axios";
 //import api from "../axiosConfig.js";
 import { Link } from "react-router-dom";
 import BookCard from "./BookCard";
+import "./fetchbooks";
 
 class ShowBookList extends Component {
   constructor(props) {
@@ -11,19 +12,6 @@ class ShowBookList extends Component {
     this.state = {
       books: [],
     };
-  }
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:8082/api/books/")
-      .then((res) => {
-        this.setState({
-          books: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log("Error from ShowBookList");
-      });
   }
 
   render() {
---
+++ src/components/fetchbooks.js
@@ -0,0 +1,15 @@
+window.onload = function fetchBooks() {
+  axios.defaults.headers.post["Content-Type"] =
+    "application/json;charset=utf-8";
+  axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+  axios
+    .get("http://localhost:8082/api/books/")
+    .then((res) => {
+      this.setState({
+        books: res.data,
+      });
+    })
+    .catch((err) => {
+      console.log("Error from ShowBookList");
+    });
+};
